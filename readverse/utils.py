61e0f815f2fee498f6453726252025d7e50e5e96
Rendy Arya Kemal <renrror@gmail.com>
2023-11-28 21:36:26 +0700

    Handle multidict to dict properly

--- readverse/utils.py
+++ readverse/utils.py
@@ -3,9 +3,23 @@ import typing as t
 from flask import Response, jsonify, request
 from pydantic import BaseModel, ValidationError
 
+from werkzeug.datastructures import MultiDict
+
 P = t.ParamSpec("P")
+K = t.TypeVar("K")
+V = t.TypeVar("V")
 TResponse = t.TypeVar("TResponse")
 ValidateModel = t.Type[BaseModel] | None
+
+
+def multidict_to_dict(multidict: MultiDict[K, V]) -> dict[K, list[V] | V]:
+    obj: dict[K, list[V] | V] = {}
+    for key, val in multidict.lists():
+        obj[key] = val
+        if len(val) == 1:
+            obj[key] = val[0]
+
+    return obj
 
 
 def validate(f: t.Callable[P, TResponse]):
@@ -18,10 +32,14 @@ def validate(f: t.Callable[P, TResponse]):
     def inner(*args: P.args, **kwargs: P.kwargs):
         try:
             if form_model:
-                kwargs["form"] = form_model.model_validate(request.form.to_dict())
+                kwargs["form"] = form_model.model_validate(
+                    multidict_to_dict(request.form)
+                )
 
             if query_model:
-                kwargs["query"] = query_model.model_validate(request.args.to_dict())
+                kwargs["query"] = query_model.model_validate(
+                    multidict_to_dict(request.args)
+                )
 
             if json_model:
                 kwargs["json"] = json_model.model_validate_json(request.data)
