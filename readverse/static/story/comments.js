f50334361f468fb3ad0d9f4397c6e5cc468d577d
Rendy Arya Kemal <renrror@gmail.com>
2023-11-30 20:53:17 +0700

    Add comment deletion

--- readverse/static/story/comments.js
+++ readverse/static/story/comments.js
@@ -1,7 +1,7 @@
 import {
   createSignal,
   createResource,
-  Suspense,
+  Show,
   For,
 } from "https://cdn.skypack.dev/solid-js";
 import { render } from "https://cdn.skypack.dev/solid-js/web";
@@ -18,9 +18,13 @@ const options = {
 
 const formatter = new Intl.DateTimeFormat("en-US", options);
 const storyId = window.location.pathname.trimEnd("/").split("/").slice(-1)[0];
+const commentDiv = document.querySelector("#comments");
+const isAdmin = commentDiv.dataset.admin == "True";
+const currentUsername = commentDiv.dataset.currentUsername;
 
 /**
  * @typedef {Object} Comment
+ * @property {string} id
  * @property {string} content
  * @property {string} timestamp
  * @property {string} username
@@ -44,14 +48,31 @@ const Comment = (props) => {
   /** @type {Comment} */
   const comment = props.comment;
 
+  async function deleteComment() {
+    await fetch(`/story/${storyId}/comment/${comment.id}/delete`, {
+      method: "DELETE",
+    });
+    alert("Successfully deleted!");
+    props.refetch();
+  }
+
   return html`<div
     class="flex flex-col gap-2 p-4 rounded-md border border-primary"
   >
     <div class="flex flex-col md:flex-row justify-between md:items-center">
       <strong class="font-bold">${comment.username}</strong>
-      <time class="text-sm text-gray-500" datetime=${comment.timestamp}
-        >${formatter.format(new Date(comment.timestamp))}</time
-      >
+      <div>
+        ${(comment.username == currentUsername || isAdmin) &&
+        html`<a
+          class="mr-4 hover:underline hover:text-gray-300"
+          onClick=${() => deleteComment()}
+        >
+          Delete
+        </a>`}
+        <time class="text-sm text-gray-500" datetime=${comment.timestamp}
+          >${formatter.format(new Date(comment.timestamp))}</time
+        >
+      </div>
     </div>
     <p>${comment.content}</p>
   </div>`;
@@ -75,21 +96,26 @@ const CommentSection = () => {
   }
 
   return html`<div class="flex flex-col gap-4">
-    <textarea
-      class="textarea textarea-bordered w-full"
-      onChange=${(e) => setContent(e.currentTarget.value)}
-    ></textarea>
-    <button
-      class="btn btn-primary w-fit ml-auto"
-      onClick=${() => postComment()}
-    >
-      Comment
-    </button>
+    ${!isAdmin &&
+    currentUsername != "" &&
+    html`<>
+      <textarea
+        class="textarea textarea-bordered w-full"
+        onChange=${(e) => setContent(e.currentTarget.value)}
+      ></textarea>
+      <button
+        class="btn btn-primary w-fit ml-auto"
+        onClick=${() => postComment()}
+      >
+        Comment
+      </button>
+    </>`}
 
     <${For} each=${() => (comments() || emptyComments).data}>
-      ${(comment) => html`<${Comment} comment=${comment} />`}
+      ${(comment) =>
+        html`<${Comment} comment=${comment} refetch=${() => refetch} />`}
     <//>
   </div>`;
 };
 
-render(CommentSection, document.querySelector("#comments"));
+render(CommentSection, commentDiv);
